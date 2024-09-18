0425d9af7b6cc0481f0854f4a5fef127148428d0
devanaclimgo <anaclimgo@gmail.com>
2024-09-18 19:47:50 -0300

    dropdown menu adicionado e estilizado

---
+++ script.js
@@ -0,0 +1,12 @@
+//dropdown menu na header
+
+const mainMenu = document.getElementById("main-menu")
+const toggleButton = document.createElement("button")
+toggleButton.textContent = "Menu";
+toggleButton.classList.add("menu-toggle");
+
+mainMenu.parentNode.insertBefore(toggleButton, mainMenu);
+
+toggleButton.addEventListener("click", function() {
+  mainMenu.classList.toggle("show");
+})
