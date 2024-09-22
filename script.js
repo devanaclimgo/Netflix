034c223769c62e882eab6e7191e920513c53f209
devanaclimgo <anaclimgo@gmail.com>
2024-09-22 19:06:16 -0300

    listas estilizadas

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //dropdown menu na header
 
-const mainMenu = document.getElementById("main-menu")
-const toggleButton = document.createElement("button")
+const mainMenu = document.getElementById("main-menu");
+const toggleButton = document.createElement("button");
 toggleButton.textContent = "Menu";
 toggleButton.classList.add("menu-toggle");
 
 mainMenu.parentNode.insertBefore(toggleButton, mainMenu);
 
-toggleButton.addEventListener("click", function() {
+toggleButton.addEventListener("click", function () {
   mainMenu.classList.toggle("show");
 
   toggleButton.classList.toggle("clicked");
-})
+});
