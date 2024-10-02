511dcfe69a86aca5759db509298898a9a156d33a
devanaclimgo <anaclimgo@gmail.com>
2024-10-02 10:14:44 -0300

    menu-toggle js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// import swiper js
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
 //dropdown menu na header
 
 const mainMenu = document.getElementById("main-menu");
@@ -20,23 +13,4 @@ toggleButton.addEventListener("click", function () {
   } else {
     mainMenu.classList.add("show");
   }
-});
-
-
-// swipper
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
